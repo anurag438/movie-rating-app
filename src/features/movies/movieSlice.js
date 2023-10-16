07ddc9bc2b84c0cd15aca7b8e3f7ac8580c60e53
anurag438 <davidanurag6@gmail.com>
2023-10-16 15:23:33 +0530

    applied middleware redux toolkit thunk, fetched movies and shows

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,7 +1,20 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createSlice,createAsyncThunk} from "@reduxjs/toolkit"
+import movieApi from "../../common/apis/MovieApi"
+import {APIKey} from '../../common/apis/MovieApiKey'
 
+export const fetchAsyncMovies = createAsyncThunk("movies/fetchAsyncMovies",async()=>{
+    const movieText = "Harry";
+    const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+    return response.data
+})
+export const fetchAsyncShows = createAsyncThunk("movies/fetchAsyncShows",async()=>{
+    const movieText = "Friends";
+    const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=series`)
+    return response.data
+})
 const initialState = {
     movies:{},
+    shows:{},
 }
 const movieSlice = createSlice({
     name:'movies',
@@ -11,7 +24,24 @@ const movieSlice = createSlice({
             state.movies = payload;
         },
     },
+    extraReducers:{
+        [fetchAsyncMovies.pending]:()=>{
+            console.log("Pending");
+        },
+        [fetchAsyncMovies.fulfilled]:(state,{payload})=>{
+            console.log("movies Fetched succesfully");
+            return {...state,movies:payload}
+        },
+        [fetchAsyncMovies.rejected]:()=>{
+            console.log("Rejected!");
+        },
+        [fetchAsyncShows.fulfilled]:(state,{payload})=>{
+            console.log("shows Fetched succesfully");
+            return {...state,shows:payload}
+        },
+    }
 })
 export const {addMovies}=movieSlice.actions;
 export const getAllMovies = (state)=>state.movies.movies;//state.movies(from name=movies).movies(from initalState)
+export const getAllShows = (state)=>state.movies.shows;//state.movies(from name=movies).movies(from initalState)
 export default movieSlice.reducer;
