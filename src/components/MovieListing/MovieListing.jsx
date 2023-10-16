3a87df5c2607a1d98650b0350c0900b4450357ce
anurag438 <davidanurag6@gmail.com>
2023-10-16 21:33:28 +0530

    added carousel using react-carousal library

--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux'
 import { getAllMovies, getAllShows } from '../../features/movies/movieSlice'
 import MovieCard from '../MovieCard/MovieCard'
 import "./MovieListing.scss"
+import Slider from 'react-slick'
+import { Settings } from '../../common/settings'
 const MovieListing = () => {
   const movies = useSelector(getAllMovies)
   const shows = useSelector(getAllShows)
@@ -18,13 +20,17 @@ renderShows = shows.Response === "True" ? (shows.Search.map((movie,index)=>{
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">
-          {renderMovies}
+          <Slider {...Settings}>
+            {renderMovies}
+          </Slider>
         </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
         <div className="movie-container">
-          {renderShows}
+          <Slider {...Settings}>
+            {renderShows}
+          </Slider> 
         </div>
       </div>
     </div>
